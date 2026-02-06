3162167f0355be437e05da32673bd46c62c79d09
Jenna <jenna.nicospace@gmail.com>
2026-02-06 10:04:03 -0500

    Employee creation & payroll

--- src/main/java/payroll/EmployeeFactory.java
+++ src/main/java/payroll/EmployeeFactory.java
@@ -1,5 +1,22 @@
 package payroll;
 
 public class EmployeeFactory {
+	
+	public Employee createEmployee(String fName, String lName, String workerType) {
+		Employee myWorker = null;
+		
+		if (workerType == "CONTRACTOR") {
+			myWorker = new ContractWorker(fName, lName);
+		}
+		else if (workerType == "HOURLY") {
+			myWorker = new HourlyWorker(fName, lName);
+		}
+		else if (workerType == "SALARY") {
+			myWorker = new SalaryWorker(fName, lName);
+		}
+		
+		return myWorker;
+		
+	}
 
 }
--- src/main/java/payroll/Payroll.java
+++ src/main/java/payroll/Payroll.java
@@ -1,9 +1,31 @@
 package payroll;
 
-public class Payroll {
+import java.util.ArrayList;
+
+public class Payroll extends EmployeeFactory {
 	
-	public void addEmployee() {
+	ArrayList<Employee> payroll;
+	
+	public Payroll() {
+		super();
+		this.payroll = new ArrayList<Employee>();
+	}
+
+	public void addNewEmployee(String fName, String lName, String workerType) {
+		Employee newEmployee = createEmployee(fName, lName, workerType);
+		payroll.add(newEmployee);
+	}
+	
+	public void setEmployeeHours(Employee employee, int hours) {
+		employee.setHours(hours);
+	}
+	
+	public int[] payEmployees() {
+		int[] weeklyPay = new int[payroll.size()];
 		
+		// Implementing later
+		
+		return weeklyPay;
 	}
 
 }
