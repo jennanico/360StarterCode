2af86966d534b264f3dff9dea76f4d2b4d8046e0
Jenna <jenna.nicospace@gmail.com>
2026-02-23 08:53:31 -0500

    ClientLogin final

--- src/main/java/client/ClientLogin.java
+++ src/main/java/client/ClientLogin.java
@@ -7,16 +7,18 @@ public class ClientLogin {
 	public static void main(String[] args) {
 		
 		RestClient defaultClient = RestClient.create();
+		
+		String username = "jenna";
 	
 		String password = defaultClient.get()
-								.uri("http://cs-hydra.centre.edu:9000/request/{username}", "jenna")
+								.uri("http://cs-hydra.centre.edu:9000/request/{username}", username)
 								.retrieve()
 								.body(String.class);
 						
 		System.out.println(password);
 		
 		String registerResult = defaultClient.get()
-									.uri("http://cs-hydra.centre.edu:9000/auth/jenna/{pw}", password)
+									.uri("http://cs-hydra.centre.edu:9000/auth/{username}/{pw}", username, password)
 									.retrieve()
 									.body(String.class);
 		
