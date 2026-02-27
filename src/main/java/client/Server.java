76572fbfa4b955d18fab22ac41a92254bb65a1bb
Jenna <jenna.nicospace@gmail.com>
2026-02-27 09:53:14 -0500

    Server final

--- src/main/java/client/Server.java
+++ src/main/java/client/Server.java
@@ -1,5 +1,8 @@
 package client;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,30 +13,50 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Server {
 	
+	static HashMap<String, String> passDatabase = new HashMap();
+	ArrayList<String> verified = new ArrayList();
+	
 	
 	@GetMapping("/request")
 	public String showRequestList() {
-		return "Hi";
+		String reqListString = passDatabase.keySet().toString();
+		return reqListString;
 		
 	}
 		
 	
 	@GetMapping("/auth")
 	public String showAuthList() {
-		return "Hi";
+		String authListString = verified.toString();
+		return authListString;
     		
     }
 	
 	@GetMapping("/request/{username}")
 	public String showPassForUser(@PathVariable String username) {
-		return "Hi";
+		if (passDatabase.containsKey(username)) {
+			return passDatabase.get(username);
+		}
+		
+		int fourDigitNumber = (int) (Math.random() * 9000) + 1000;
+		passDatabase.put(username, String.valueOf(fourDigitNumber));
+		return String.valueOf(fourDigitNumber);
 		
 	}
 	
 	@GetMapping("/auth/{username}/{password}")
 	public String authPassForUser(@PathVariable String username, @PathVariable String password) {
-		return "Hi";
+		if (passDatabase.get(username).equals(password)) {
+			
+			if (!verified.contains(username)) {
+				verified.add(username);
+			}
+			
+			return "Authenticated!";
+		}
 		
+		return "Authentication failed";
+
 	}
 	
 	
@@ -48,12 +71,12 @@ public class Server {
 				   "<li><a href='/request'>/request</a> a list of all people who have requested passwords</li>" +
 				   "<li><a href='/auth'>/auth</a> a list of all people who have been successfully authenticated</li>" +
 				   "<li><a href='/request/janedoe'>/request/:username</a> a request for a password for the username provided</li>" +
-				   "<li><a href='/auth/janedoe/3104'>/auth/:username/:password</a> attempts to authenticate the password for the username provided</li>";
+				   "<li><a href='/auth/janedoe/4132'>/auth/:username/:password</a> attempts to authenticate the password for the username provided</li>";
 		return index;
 	}
 	
-	
 	public static void main(String[] args) {
+		passDatabase.put("janedoe", "4132");
 		SpringApplication.run(Server.class, args);
 	}
 	    
